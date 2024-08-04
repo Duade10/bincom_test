2004e97a79175a212046653362b613f4d0fd0fc2
Duade10 <otunlaisaac8@gmail.com>
2024-08-04 21:25:04 +0100

    to add get mean colour function
    This pull request adds a function to calculate the mean color from the table.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def read_html_file(path_to_html_file):
@@ -7,8 +8,26 @@ def read_html_file(path_to_html_file):
     return html
 
 
+def extract_colours(html_content):
+    colours = re.findall(r'\b[A-Z]+\b', html_content)
+    colours = [colour for colour in colours if
+               colour not in ['DAY', 'COLOURS', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY']]
+    return colours
+
+
+def get_mean_colour(colours):
+    colour_count = Counter(colours)
+    colour_frequency = sum(colour_count.values() / len(colour_count))
+    mean_colour = min(colour_count, key=lambda x: abs(colour_count[x] - colour_frequency))
+    return mean_colour
+
+
 HTML_FILE_PATH = 'python_class_question.html'
 
 html_content = read_html_file(HTML_FILE_PATH)
 
-print(html_content)
+colours = extract_colours(html_content)
+print(colours)
+
+mean_colour = get_mean_colour(colours)
+print(mean_colour)
